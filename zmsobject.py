d78bddf87af48095eb4ae2b796deb2ef89e7221e
Dr. Frank Hoffmann <fh@sntl-publishing.com>
2015-07-23 13:58:25 +0000

    added custom zmi rendering via renderShort() to all content defs (contrib. by N Dettenbach)
    svn path=/ZMS/branches/zms2/; revision=3213

--- zmsobject.py
+++ zmsobject.py
@@ -1346,11 +1346,11 @@ class ZMSObject(ZMSItem.ZMSItem,
       """
       html = ''
       try:
-        if self.getType() in [ 'ZMSDocument', 'ZMSResource', 'ZMSReference']:
+        if 'renderShort' in self.getMetaobjAttrIds(self.meta_id):
+          html = self.attr('renderShort')
+        elif self.getType() in [ 'ZMSDocument', 'ZMSResource', 'ZMSReference']:
           html = self.getTitlealt(REQUEST)
           html = '<div class="contentEditable form-label" id="contentEditable_%s_%s">%s</div>'%(self.id,REQUEST['lang'],html)
-        elif 'renderShort' in self.getMetaobjAttrIds(self.meta_id):
-          html = self.attr('renderShort')
         else:
           html = self._getBodyContent(REQUEST)
         html = '<div class="zmiRenderShort">%s</div><!-- .zmiRenderShort -->'%html
